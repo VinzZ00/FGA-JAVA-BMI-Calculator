bba783e0b76d740fa17f730bda349e3556f66e61
Senna Annaba Ahmad <sennaannaba@gmail.com>
2022-07-28 15:48:45 +0700

    Buat Main Method Clean

--- src/main/java/com/mycompany/bmiapplication/mainEntrance.java
+++ src/main/java/com/mycompany/bmiapplication/mainEntrance.java
@@ -5,8 +5,8 @@
  */
 package com.mycompany.bmiapplication;
 
+import com.mycompany.bmiapplication.windows.Login;
 import javafx.application.Application;
-import static javafx.application.Application.launch;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
@@ -30,50 +30,8 @@ public class mainEntrance extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        
-        Label headerLabel = new Label("Welcome To Login Page");
-        headerLabel.setFont(new Font("Sans Serif", 25));
-        Button loginBut = new Button("Login");
-        
-        GridPane form = new GridPane();
-        form.setAlignment(Pos.CENTER);
-        form.setHgap(40);
-        form.setVgap(20);
-        Label emailLabel = new Label("Email");
-        TextField emailField = new TextField();
-        
-        Label passwordLabel = new Label("Password");
-        PasswordField passwordField = new PasswordField();
-        
-        form.add(emailLabel, 0, 0);
-        form.add(emailField, 1, 0);
-        form.add(passwordLabel, 0, 1);
-        form.add(passwordField, 1, 1);
-        
-        BorderPane.setAlignment(loginBut,Pos.BOTTOM_CENTER);
-        BorderPane.setAlignment(form,Pos.CENTER);
-        BorderPane.setAlignment(headerLabel,Pos.TOP_CENTER);
-        
-        BorderPane root = new BorderPane(form, headerLabel, null, loginBut, null);
-        
-        
-        root.setMargin(loginBut, new Insets(10,10,10,10));
-        root.setPadding(new Insets(10, 0, 0, 10));
-        
-        
-        loginBut.setOnAction((e) -> {
-            
-        });
-        
-        
-        Scene loginScene = new Scene(root);
-
-        primaryStage.setWidth(400);
-        primaryStage.setHeight(300);
-        primaryStage.setTitle("Login Page");    
-        primaryStage.setScene(loginScene);
-        primaryStage.show();
+    public void start(Stage primaryStage) {
+        Login.render(primaryStage);
     }
  
     
